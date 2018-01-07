0f3a408dc4e6adc125eed2ad732bd3250a5b5130
Jingyi Isaacs <shizuwang1209@gmail.com>
2018-01-06 23:16:25 -0800

    feat: Refactor the code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-// Use EventEmitter to decouple functions completely
+// Use EventEmitter to decouple functions
 var EVT = new EventEmitter2();
-
-
     Cards.run();
-    Moves.run();
-    Stars.run();
-    Timer.run();
-    Congrats.run();
+    Performance.run();
